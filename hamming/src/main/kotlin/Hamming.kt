1f94239899c4642cd61d2f53bf65ecfc089abeac
이지영 <violentjy@gmail.com>
2019-03-05 22:27:24 +0900

    Hamming - self comment for study

--- hamming/src/main/kotlin/Hamming.kt
+++ hamming/src/main/kotlin/Hamming.kt
@@ -1,12 +1,13 @@
 import java.lang.IllegalArgumentException
 
 class Hamming {
-    companion object {
+    companion object { // Singletone 으로 선언했으면 간단!
         fun compute(left: String, right: String) : Int {
             if (left.length != right.length) {
                 throw IllegalArgumentException("left and right strands must be of equal length.")
             }
 
+            // 이하 zip 활용했다면 아주 깔-끔하게 처리 가능!
             var num = 0
 
             for (i in left.toCharArray().indices) {
