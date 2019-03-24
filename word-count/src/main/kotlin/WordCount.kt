396abcdafd21f1149461732808acbcc9dc363180
이지영 <violentjy@gmail.com>
2019-03-24 21:30:29 +0900

    WordCount - self comment for study

--- word-count/src/main/kotlin/WordCount.kt
+++ word-count/src/main/kotlin/WordCount.kt
@@ -1,8 +1,18 @@
 object WordCount {
     fun phrase(input: String): Map<String, Int> {
+        // 1. toLowerCase 를 먼저 해줘도 된다
+        // 2. 1의 방법으로 했으면 정규식에서 A-Z 가 필요없다.
+        // 3. groupBy & mapValues 라는 함수가 있다...! 헉
         val words = input.split("""[^a-zA-Z0-9]{2,}|[^'a-zA-Z0-9]+""".toRegex())
                         .filter { it.isNotEmpty() }
                         .map { it.toLowerCase() }
+
+        // 아래는 수정
+        val result = input.toLowerCase()
+                .split("""[^a-z0-9]{2,}|[^'a-z0-9]+""".toRegex())
+                .filter { it.isNotEmpty() }
+                .groupBy { it }
+                .mapValues { it.value.size }
 
         return words.phrase()
     }
